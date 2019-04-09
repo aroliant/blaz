45f0dee9183314d1eb252bcc4717ea2ca53b3550
Manooj Kumar <manoojkumar@aroliant.com>
2019-04-09 13:48:27 +0000

    Update install-dev-env.sh

--- dev-scripts/install-dev-env.sh
+++ dev-scripts/install-dev-env.sh
@@ -15,28 +15,26 @@ sudo apt-get install \
 echo "Adding Docker APT Key"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-echo "Adding Docker APT Repository"
+echo "Adding Docker to APT Repository"
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-echo "Update the apt package"
+echo "Updating APT Index"
 sudo apt-get update
 
 echo "Install latest version of Docker CE"
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-echo "Steps to access Docker Engine remote API"
-echo "Creating an folder for Docker Engine Service"
+echo "Enabeling Docker engine API"
+
 sudo mkdir /etc/systemd/system/docker.service.d
 
-echo "Creating an file to run Docker Engine API"
 sudo touch /etc/systemd/system/docker.service.d/startup_options.conf
 
 echo "# /etc/systemd/system/docker.service.d/override.conf\n[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2376" > /etc/systemd/system/docker.service.d/startup_options.conf
 
-echo "Successfully added a file"
 echo "Reload Unit files"
 sudo systemctl daemon-reload
 
@@ -48,5 +46,5 @@ echo "Successfully restarted Docker Engine API and Running in port 2376 "
 echo "Creating Docker Volume for Portainer"
 sudo docker volume create portainer_data
 
-echo "Run Portainer default at port 9000"
+echo "Running Portainer  at port 9000"
 sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
