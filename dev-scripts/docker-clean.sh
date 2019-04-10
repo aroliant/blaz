a1e05dba94abd4a5a7452e2a6c4b0ac8db2230c0
Manooj Kumar <manoojkumar@aroliant.com>
2019-04-10 10:09:09 +0000

    Update docker-clean.sh

--- dev-scripts/docker-clean.sh
+++ dev-scripts/docker-clean.sh
@@ -3,4 +3,11 @@ echo "Removing all Image,Container,Volumes,Network and Services"
 sudo docker stop $(docker ps -a -q)
 echo "y" | sudo docker system prune -a
 echo "y" | sudo docker volume prune
-sudo service docker stop
+
+echo "Removing Docker Engine API"
+sudo rm /etc/systemd/system/docker.service.d/startup_options.conf
+sudo rmdir /etc/systemd/system/docker.service.d
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker.service
+
