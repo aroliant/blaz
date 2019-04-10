f70dd0d6ff8497548c7c72579dd4ea831e1b61bb
Manooj Kumar <manoojkumar@aroliant.com>
2019-04-10 10:01:25 +0000

    Update docker-clean.sh

--- dev-scripts/docker-clean.sh
+++ dev-scripts/docker-clean.sh
@@ -1,5 +1,6 @@
-echo "Remove All Unused Image,Container,Volumes and Network"
+echo "Removing all Image,Container,Volumes,Network and Services"
 
 sudo docker stop $(docker ps -a -q)
 echo "y" | sudo docker system prune -a
 echo "y" | sudo docker volume prune
+sudo service docker stop
