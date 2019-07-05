7825a5eecee7ab9bcecc0294cc0703b9cb34f05e
Jacob Samro <jacobsamro@outlook.com>
2019-07-05 18:11:14 +0530

    Fix: Port Issue, Dev Support for Mac & Cent OS

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ if (!EnvVars.IS_BLAZ_INSTANCE) {
   // eslint-disable-next-line global-require
   const app = require('./server/blazApp')
 
-  const port = parseInt(process.env.PORT, 3000)
+  const port = process.env.PORT || 3000
+  //port = parseInt(port, 3000)
+  console.log("PORT ___ PORT", port)
   app.set('port', port)
 
   const server = http.createServer(app)
