c1f5fa496e24850be393b5f95c223b7a7d4b8867
Jacob Samro <jacobsamro@outlook.com>
2019-07-05 16:04:19 +0530

    refact: app.js - remove unwanted functions

--- app.js
+++ app.js
@@ -1,55 +1,37 @@
+import http from 'http'
 import Logger from './server/utils/Logger';
 import AppConstants from './server/config/AppConstants';
 import BlazInstaller from './server/utils/BlazInstaller';
 import BlazApiServer from './server/services/BlazApiServer'
 import EnvVars from './server/utils/EnvVars'
-import http from 'http'
 
 
 const blazInstaller = new BlazInstaller()
 const blazApiServer = new BlazApiServer()
 
 if (!EnvVars.IS_BLAZ_INSTANCE) {
-  Logger.info("Installing Blaz")
+  Logger.info('Installing Blaz')
   blazInstaller.install() // Start the installer if Blaz is not installed
 
 } else {
+  // eslint-disable-next-line global-require
   const app = require('./server/blazApp')
 
-  var port = normalizePort(process.env.PORT || '3000')
+  const port = parseInt(process.env.PORT, 3000)
   app.set('port', port)
 
-  var server = http.createServer(app)
+  const server = http.createServer(app)
 
   server.listen(port)
-  server.on('error', onError)
-  server.on('listening', onListening)
+  server.on('error', (err) => {
+    Logger.log('Error on http ->  start', err)
+  })
+  server.on('listening', (err) => {
+    Logger.log('Error on http -> listening', err)
+  })
 
 }
 
-function normalizePort(val) {
-  var port = parseInt(val, 10)
-
-  if (isNaN(port)) {
-    return val
-  }
-
-  if (port >= 0) {
-    return port
-  }
-
-  return false
-
-}
-
-function onError(error) {
-  Logger.log('Error')
-}
-
-
-function onListening() {
-  Logger.log('Listening')
-}
 
 /*
 
