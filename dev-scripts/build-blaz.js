fa190ae6770e0f7121ebeb3940e1c42fbea19af8
Jacob Samro <jacobsamro@outlook.com>
2019-07-12 00:00:50 +0530

    Local Build Script Added

--- dev-scripts/build-blaz.js
+++ dev-scripts/build-blaz.js
@@ -1,6 +1,6 @@
 const shell = require('shelljs')
 const fs = require('fs')
 
-const BLAZ_IMAGE_TAG = fs.readFileSync(__dirname + "/../server/env").toString().split("=")[1]
+const BLAZ_IMAGE_TAG = fs.readFileSync(__dirname + "/../server/env").toString().trim().split("=")[1]
 
 shell.exec(`docker build -t blaz/blaz:${BLAZ_IMAGE_TAG} .`)
