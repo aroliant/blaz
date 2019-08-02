867c86a67f4438ac3d19e28956f63f4cae7dfae3
Jacob Samro <jacobsamro@outlook.com>
2019-08-02 16:25:51 +0530

    Dashboard : UI Functionalities for Port Mapping & Environment Variables

--- client/app/pages/apps/detail/components/app-configuration/app-configuration.component.ts
+++ client/app/pages/apps/detail/components/app-configuration/app-configuration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AppService } from 'client/app/services/app.service';
 
 @Component({
   selector: 'app-detail-app-configuration',
@@ -11,19 +12,51 @@ export class AppConfigurationComponent implements OnInit {
   @Input() app;
   showTab = false
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
 
-  ngOnInit(){
-    
+  ngOnInit() {
+    if (!this.app.environmentVars) {
+      this.app.environmentVars = [{
+        key: '', value: ''
+      }]
+    }
+
+    if (!this.app.portMapping) {
+      this.app.portMapping = [
+        {
+          hostPort: 8080,
+          containerPort: 80
+        }
+      ]
+    }
+
   }
 
   ngOnChanges() {
-    this.showTab = this.activeTab == "app-configuration" ? true : false 
+    this.showTab = this.activeTab == "app-configuration" ? true : false
   }
 
-  saveAndUpdate(){
-    console.log(this.app)
+  saveAndUpdate() {
+    this.appService.updateApp(this.app).subscribe((res: any) => {
+
+    })
+  }
+
+  enterEnvVars(e, index) {
+    if (e.keyCode == 13 && index == this.app.environmentVars.length - 1) {
+      this.app.environmentVars.push({
+        key: '',
+        value: ''
+      })
+    }
+  }
+
+  addNewPortMapping() {
+    this.app.portMapping.push({
+      hostPort: '',
+      containerPort: ''
+    })
   }
 
 }
