62b3e78ab7635e981eaf7a2657778874cd0ad4b1
Jacob Samro <jacobsamro@outlook.com>
2019-08-09 11:54:00 +0530

    feat(pre-commit,commit-msg):  validation script

---
+++ dev-scripts/pre-commit.js
@@ -0,0 +1,5 @@
+// TODO
+// There should not be a package-lock.json
+// Check if there are changes in environment / config.js / env files
+// Check if build files such as app.bin / build folders are staged
+// There should not be files in temp_upload
